61fc14bee9013ccd086242fd3dee4f0147b4e2b0
Pascal Olin <Pascal.Olin@serco.com>
2019-10-31 17:02:06 +0100

    first PO commit

---
+++ SimpleQRCodeGenerator/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace SimpleQRCodeGenerator
+{
+    partial class SercoQRCodeGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SercoQRCodeGenerator));
+            this.SourceText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.wifiSSID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.wifiPassword = new System.Windows.Forms.TextBox();
+            this.listOfSSIDs = new System.Windows.Forms.ListBox();
+            this.getSSIDs = new System.Windows.Forms.Button();
+            this.listOFPasswords = new System.Windows.Forms.ListBox();
+            this.listOfAuths = new System.Windows.Forms.ListBox();
+            this.wifiAuth = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.saveToSettings = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SourceText
+            // 
+            this.SourceText.Location = new System.Drawing.Point(127, 65);
+            this.SourceText.Name = "SourceText";
+            this.SourceText.Size = new System.Drawing.Size(612, 26);
+            this.SourceText.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter ANY Text/URL below and press Generate ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(330, 94);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate QRCode and  then click on the image to save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(127, 302);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(271, 247);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(483, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Fit Code to Size (smaller)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // wifiSSID
+            // 
+            this.wifiSSID.Location = new System.Drawing.Point(127, 138);
+            this.wifiSSID.Name = "wifiSSID";
+            this.wifiSSID.Size = new System.Drawing.Size(285, 26);
+            this.wifiSSID.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Wifi  SSID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Wifi  Password";
+            // 
+            // wifiPassword
+            // 
+            this.wifiPassword.Location = new System.Drawing.Point(127, 170);
+            this.wifiPassword.Name = "wifiPassword";
+            this.wifiPassword.Size = new System.Drawing.Size(285, 26);
+            this.wifiPassword.TabIndex = 7;
+            // 
+            // listOfSSIDs
+            // 
+            this.listOfSSIDs.FormattingEnabled = true;
+            this.listOfSSIDs.ItemHeight = 20;
+            this.listOfSSIDs.Location = new System.Drawing.Point(776, 138);
+            this.listOfSSIDs.Name = "listOfSSIDs";
+            this.listOfSSIDs.Size = new System.Drawing.Size(247, 424);
+            this.listOfSSIDs.TabIndex = 10;
+            this.listOfSSIDs.Click += new System.EventHandler(this.listOfSSIDs_Click);
+            this.listOfSSIDs.SelectedIndexChanged += new System.EventHandler(this.listOfSSIDs_SelectedIndexChanged);
+            this.listOfSSIDs.DoubleClick += new System.EventHandler(this.listOfSSIDs_DoubleClick);
+            // 
+            // getSSIDs
+            // 
+            this.getSSIDs.Location = new System.Drawing.Point(776, 102);
+            this.getSSIDs.Name = "getSSIDs";
+            this.getSSIDs.Size = new System.Drawing.Size(247, 30);
+            this.getSSIDs.TabIndex = 11;
+            this.getSSIDs.Text = "Get and store known networks";
+            this.getSSIDs.UseVisualStyleBackColor = true;
+            this.getSSIDs.Click += new System.EventHandler(this.getSSIDs_Click);
+            // 
+            // listOFPasswords
+            // 
+            this.listOFPasswords.FormattingEnabled = true;
+            this.listOFPasswords.ItemHeight = 20;
+            this.listOFPasswords.Location = new System.Drawing.Point(868, 396);
+            this.listOFPasswords.Name = "listOFPasswords";
+            this.listOFPasswords.Size = new System.Drawing.Size(247, 324);
+            this.listOFPasswords.TabIndex = 12;
+            this.listOFPasswords.Visible = false;
+            // 
+            // listOfAuths
+            // 
+            this.listOfAuths.FormattingEnabled = true;
+            this.listOfAuths.ItemHeight = 20;
+            this.listOfAuths.Location = new System.Drawing.Point(831, 318);
+            this.listOfAuths.Name = "listOfAuths";
+            this.listOfAuths.Size = new System.Drawing.Size(247, 324);
+            this.listOfAuths.TabIndex = 13;
+            this.listOfAuths.Visible = false;
+            // 
+            // wifiAuth
+            // 
+            this.wifiAuth.Location = new System.Drawing.Point(440, 167);
+            this.wifiAuth.Name = "wifiAuth";
+            this.wifiAuth.Size = new System.Drawing.Size(120, 26);
+            this.wifiAuth.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(436, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Auth(WPA,WPA2,WEP)";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(85, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(500, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Or select a SSID to publish as QR code in the list and press Generate";
+            // 
+            // saveToSettings
+            // 
+            this.saveToSettings.Location = new System.Drawing.Point(776, 568);
+            this.saveToSettings.Name = "saveToSettings";
+            this.saveToSettings.Size = new System.Drawing.Size(247, 30);
+            this.saveToSettings.TabIndex = 17;
+            this.saveToSettings.Text = "Save ALL SSIDs to Settings";
+            this.saveToSettings.UseVisualStyleBackColor = true;
+            this.saveToSettings.Click += new System.EventHandler(this.saveToSettings_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(776, 595);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(247, 30);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Empty Settings of ALL SSIDs";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(518, 276);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(252, 20);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Double click to remove one entry->";
+            // 
+            // SercoQRCodeGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 637);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.saveToSettings);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.wifiAuth);
+            this.Controls.Add(this.listOfAuths);
+            this.Controls.Add(this.listOFPasswords);
+            this.Controls.Add(this.getSSIDs);
+            this.Controls.Add(this.listOfSSIDs);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.wifiPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.wifiSSID);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SourceText);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SercoQRCodeGenerator";
+            this.Text = "SercoQRCodeGenerator";
+            this.Load += new System.EventHandler(this.SercoQRCodeGenerator_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SourceText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox wifiSSID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox wifiPassword;
+        private System.Windows.Forms.ListBox listOfSSIDs;
+        private System.Windows.Forms.Button getSSIDs;
+        private System.Windows.Forms.ListBox listOFPasswords;
+        private System.Windows.Forms.ListBox listOfAuths;
+        private System.Windows.Forms.TextBox wifiAuth;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button saveToSettings;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ SimpleQRCodeGenerator/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace SimpleQRCodeGenerator
+{
+    public partial class SercoQRCodeGenerator : Form
+    {
+        StringCollection mypasswords;
+        public SercoQRCodeGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap qrCodeImage = null;
+            if (wifiSSID.Text.Length == 0)
+            {
+
+
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                //# QRCodeData qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(SourceText.Text.ToString(), QRCodeGenerator.ECCLevel.Q);
+
+                QRCode qrCode = new QRCode(qrCodeData);
+                qrCodeImage = qrCode.GetGraphic(20);
+            }
+            else
+            {
+                //string authType = "";
+                PayloadGenerator.WiFi.Authentication authType = PayloadGenerator.WiFi.Authentication.WPA;
+                switch (wifiAuth.Text.ToUpper())
+                {
+                    case "WPA":
+                        authType = PayloadGenerator.WiFi.Authentication.WPA;
+                        break;
+                    case "WEP":
+                        authType = PayloadGenerator.WiFi.Authentication.WEP;
+                        break;
+
+
+
+                }
+
+                PayloadGenerator.WiFi generator = new PayloadGenerator.WiFi(wifiSSID.Text, wifiPassword.Text.ToString(), authType);
+                string payload = generator.ToString();
+
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+                var qrCodeAsBitmap = qrCode.GetGraphic(20);
+                qrCodeImage = qrCodeAsBitmap;
+                //pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            }
+            pictureBox1.Image = qrCodeImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            //pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var fd = new SaveFileDialog();
+            fd.Filter = "Bmp(*.BMP;)|*.BMP;| Jpg(*Jpg)|*.jpg";
+            if (fd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                switch (Path.GetExtension(fd.FileName))
+                {
+                    case ".BMP":
+                        pictureBox1.Image.Save(fd.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case ".JPG":
+                        pictureBox1.Image.Save(fd.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void SercoQRCodeGenerator_Load(object sender, EventArgs e)
+        {
+            try {
+                StringCollection mysettings = Properties.Settings.Default.one;
+                listOfSSIDs.SelectionMode = SelectionMode.One;
+                int m = 0;
+                if (mysettings.Count > 0)
+                {
+                    foreach (string setting in mysettings)
+
+                    {
+                        string[] _mysetting = setting.Split(",".ToCharArray());
+
+                        listOfSSIDs.Items.Add(_mysetting[0].Trim());
+                        listOFPasswords.Items.Add(_mysetting[1].Trim());
+                        listOfAuths.Items.Add(_mysetting[2].Trim());
+                        Console.WriteLine(setting);
+                        // to be done later listOfSSIDs.Items.Add(setting);
+                        //mypasswords.Add(setting);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception detected whilst reading ssid settings", "Exception text " + ex.Message);
+            }
+            } 
+        
+
+        private void listOfSSIDs_Click(object sender, EventArgs e)
+        {
+            // wifiSSID.Text = listOfSSIDs.SelectedItems.ToString();
+            int ndx = listOfSSIDs.SelectedIndex;
+            wifiSSID.Text = listOfSSIDs.Items[ndx].ToString();
+            wifiPassword.Text = listOFPasswords.Items[ndx].ToString();
+            wifiAuth.Text = listOfAuths.Items[ndx].ToString();
+
+        }
+
+        private void getSSIDs_Click(object sender, EventArgs e)
+        {
+            string[] _myuserprofiles = new String[100];
+            string[] _myssids = new String[100];
+            string[] _mypasswords = new String[100];
+            string[] _myauth = new String[100];
+            
+            // flush the listboxes to avoid pointers issues 
+            // to be done 
+            //listOfSSIDs.empty 
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe");
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.UseShellExecute = false;
+            Process process = Process.Start(processStartInfo);
+            process.StandardInput.WriteLine("netsh wlan show profile");
+            process.StandardInput.Close();
+            string[] Lines = process.StandardOutput.ReadToEnd().Split("\r\n".ToCharArray());
+            string LineString = string.Empty;
+            int m = -1;
+            for (int i = 0; i < Lines.Length; i++)
+            {
+                LineString = Lines[i];
+                if (LineString.Contains("User Profile"))
+                {
+                    m++;
+                    _myuserprofiles[m] = LineString.Split(":".ToCharArray())[1].Trim();
+                    _myuserprofiles[m] = "\"" + _myuserprofiles[m] + "\"";
+                    // set default values so that the reference by index later doesn't find a null value
+                    _myssids[m] = "noSSID";
+                    _mypasswords[m] = "noPass";
+                    _myauth[m] = "noAuth";
+                    ProcessStartInfo processStartInfo2 = new ProcessStartInfo("cmd.exe");
+                    processStartInfo.RedirectStandardInput = true;
+                    processStartInfo.RedirectStandardOutput = true;
+                    processStartInfo.CreateNoWindow = true;
+                    processStartInfo.UseShellExecute = false;
+                    Process process2 = Process.Start(processStartInfo);
+                    process2.StandardInput.WriteLine("netsh wlan show profile " + _myuserprofiles[m] + " key=clear");
+                    process2.StandardInput.Close();
+                    string[] Lines2 = process2.StandardOutput.ReadToEnd().Split("\r\n".ToCharArray());
+                    string LineString2 = string.Empty;
+                    int mm = 0;
+                    for (int j = 0; j < Lines2.Length; j++)
+                    {
+                        LineString2 = Lines2[j];
+                        if (LineString2.Contains("SSID name"))
+                        {
+                            // trim funny characters sent back by CMD
+                            _myssids[m] = LineString2.Split(":".ToCharArray())[1].Trim(new Char[] { ' ', '*', '"' });
+                        }
+                        if (LineString2.Contains("Key Content"))
+                        {
+                            _mypasswords[m] = LineString2.Split(":".ToCharArray())[1].Trim();
+                        }
+                        if (LineString2.Contains("Authentication"))
+                        {
+                            _myauth[m] = LineString2.Split(":".ToCharArray())[1].Trim();
+                        }
+                    }
+                    // write this line only if it was a user profile line
+                    if (_mypasswords[m] != "noPass") {
+                        Console.WriteLine(_myuserprofiles[m] + " " + _myssids[m] + " " + _mypasswords[m] + " " + _myauth[m]);
+                        listOfSSIDs.Items.Add(_myssids[m]);
+                        listOFPasswords.Items.Add(_mypasswords[m] ?? "zeroPass");
+                        listOfAuths.Items.Add(_myauth[m] ?? "zeroAuth");
+                    }
+                    else
+                    {
+                        m--;
+                    }
+                }
+                
+            }
+        }
+
+        private void listOfSSIDs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToSettings_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    StringCollection mysettings = Properties.Settings.Default.one;
+            //    listOfSSIDs.SelectionMode = SelectionMode.One;
+            //    int m = 0;
+            //    if (mysettings.Count > 0)
+            //    {
+            //        foreach (string setting in mysettings)
+
+            //        {
+            //            string[] _mysetting = setting.Split(",".ToCharArray());
+
+            //            listOfSSIDs.Items.Add(_mysetting[0].Trim());
+            //            listOFPasswords.Items.Add(_mysetting[1].Trim());
+            //            listOfAuths.Items.Add(_mysetting[2].Trim());
+            //            Console.WriteLine(setting);
+            //            // to be done later listOfSSIDs.Items.Add(setting);
+            //            //mypasswords.Add(setting);
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Exception detected whilst reading ssid settings", "Exception text " + ex.Message);
+            //}
+
+            StringCollection mysettings = Properties.Settings.Default.one;
+            mysettings.Clear();
+            //Properties.Settings.Default.Save();
+
+            int m = listOfSSIDs.Items.Count;
+            int n = -1; 
+            foreach (string _ssid in listOfSSIDs.Items)
+            {
+                n++;
+                mysettings.Add(listOfSSIDs.Items[n].ToString() + ',' + listOFPasswords.Items[n].ToString() + ',' + listOfAuths.Items[n].ToString());
+
+
+            }
+            Properties.Settings.Default.Save();
+        }
+
+        private void listOfSSIDs_DoubleClick(object sender, EventArgs e)
+        {
+            int _m = listOfSSIDs.SelectedIndex;
+            if (MessageBox.Show("Sure you want to delete ? " + listOfSSIDs.Items[_m],"delete entry",MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                listOfSSIDs.Items.RemoveAt(_m);
+                listOFPasswords.Items.RemoveAt(_m);
+                listOfAuths.Items.RemoveAt(_m);
+
+            }
+            Properties.Settings.Default.Reset();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Reset();
+        }
+    }
+}
